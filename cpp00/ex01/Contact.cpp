06a26434502f710bb4637200750e6b9b044a8e54
Martim Cardoso <mcardoso@c1r10s5.42lisboa.com>
2026-06-16 18:27:25 +0100

    added the set and get funcs in contacts and filled a bit more of the classes

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -1,2 +1,43 @@
 #include "Contact.hpp"
 
+Contact::Contact(){}
+
+std::string	Contact::getFirst() {
+	return firstname;
+}
+
+std::string	Contact::getLast() {
+	return lastname;
+}
+
+std::string	Contact::getNick() {
+	return nickname;
+}
+
+std::string	Contact::getNumber() {
+	return phonenumber;
+}
+
+std::string	Contact::getSecret() {
+	return darkestsecret;
+}
+
+void	Contact::setFirst(const std::string& firstname) {
+	this->firstname = firstname;
+}
+
+void	Contact::setLast(const std::string& lastname) {
+	this->lastname = lastname;
+}
+
+void	Contact::setNick(const std::string& nickname) {
+	this->nickname = nickname;
+}
+
+void	Contact::setNumber(const std::string& phonenumber) {
+	this->phonenumber = phonenumber;
+}
+
+void	Contact::setSecret(const std::string& darkestsecret) {
+	this->darkestsecret = darkestsecret;
+}
