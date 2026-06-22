4b75c0c30f5a849ca966b3721f90e743699e2769
Martim Cardoso <mcardoso@c1r9s3.42lisboa.com>
2026-06-22 18:47:39 +0100

    done ready to deliver

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -1,7 +1,9 @@
 #include "Zombie.hpp"
 
 int main() {
+	std::cout << "Chump is created on the stack" << std::endl;
 	randomChump("Chump");
+	std::cout << "Dave is created on the heap" << std::endl;
 	Zombie *zombie = newZombie("STEVE");
 	zombie->announce();
 	delete zombie;
