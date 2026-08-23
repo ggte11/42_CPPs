30cab68e22f12cd24a2a37a56fe88dfdec73069c
martim cardoso <martim.cardoso55@gmail.com>
2026-08-23 13:40:20 +0100

    almost finished ex01

--- cpp05/ex01/Form.cpp
+++ cpp05/ex01/Form.cpp
@@ -1,5 +1,42 @@
 #include "Form.hpp"
 
-Form::Form() {}
+Form::Form(std::string name, int grade_sign, int grade_exec) : _name(name), _grade_sign(grade_sign), _grade_exec(grade_exec), is_signed(false) {
 
-Form::~Form() {}
+}
+
+Form::Form(const Form &other) : _name(other.) {
+
+}
+
+Form::~Form() {
+}
+
+Form &Form::operator=(const Form &other) {
+	if (this != &other)
+		this->_is_signed = other.
+}
+
+std::string Form::getName() const {
+	return (_name);
+}
+
+int Form::getGradeSign() const {
+	return (_grade_sign);
+}
+
+int Form::getGradeExec() const {
+	return (_grade_exec);
+}
+
+bool Form::Is_Signed() const {
+	return (_is_signed);
+}
+
+std::ostream &operator<<(std::ostream &out, const Form &obj) {
+	out << obj.getName() << ": Grade to sign " << obj.getGradeSign() << ", Grade to execute: " << obj.getGradeExec() << ", ";
+	if (obj.Is_Signed())
+		out << "Form is signed.";
+	else
+		out << "Form is not signed.";
+	return out;
+}
--- cpp05/ex01/Form.hpp
+++ cpp05/ex01/Form.hpp
@@ -3,17 +3,26 @@
 
 #include <iostream>
 #include <string>
+#include "Bureaucrat.hpp"
 
 class Form
 {
 	private:
 		const std::string _name;
-		bool is_signed;
+		bool _is_signed;
 		const int _grade_sign;
 		const int _grade_exec;
 	public:
-		Form();
+		Form(std::string name, int grade_sign, int grade_exec);
+		Form(const Form &other);
+		Form &operator=(const Form &other);
 		~Form();
+		std::string getName() const;
+		int getGradeSign() const;
+		int getGradeExec() const;
+		bool Is_Signed() const;
 };
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &obj);
 
 #endif
