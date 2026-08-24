6e8d50f8bc0f56c17c95d489ff6bea269fc6e0e3
martim cardoso <martim.cardoso55@gmail.com>
2026-08-24 12:39:22 +0100

    almost finished

--- cpp05/ex01/Form.cpp
+++ cpp05/ex01/Form.cpp
@@ -1,20 +1,22 @@
 #include "Form.hpp"
 
-Form::Form(std::string name, int grade_sign, int grade_exec) : _name(name), _grade_sign(grade_sign), _grade_exec(grade_exec), is_signed(false) {
+Form::Form(std::string name, int grade_sign, int grade_exec) : _name(name), _grade_sign(grade_sign), _grade_exec(grade_exec), _is_signed(false) {
 
 }
 
-Form::Form(const Form &other) : _name(other.) {
+Form::Form(const Form &other) : _name(other.getName()), _grade_sign(other.getGradeSign()), _grade_exec(other.getGradeExec()), _is_signed(false) {
 
+}
+
+Form &Form::operator=(const Form &other) {
+	if (this != &other)
+		this->_is_signed = other.Is_Signed();
+	return *this;
 }
 
 Form::~Form() {
 }
 
-Form &Form::operator=(const Form &other) {
-	if (this != &other)
-		this->_is_signed = other.
-}
 
 std::string Form::getName() const {
 	return (_name);
